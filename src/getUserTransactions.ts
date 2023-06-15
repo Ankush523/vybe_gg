2fdb25669119096a5d22766c350d36510156178c
Shlok D. Mange <shlok_mange@protonmail.com>
2023-06-15 08:42:17 +0530

    improving types

--- src/getUserTransactions.ts
+++ src/getUserTransactions.ts
@@ -1,9 +1,10 @@
-import axios from 'axios';
-import { config } from 'dotenv';
-config();
+import axios from 'axios'
+import { config } from 'dotenv'
+config()
 
-const API_KEY : any = process.env.API_KEY;
-const USER_ADDRESS : any = process.env.USER_ADDRESS;
+const API_KEY = process.env.API_KEY as string
+const USER_ADDRESS = process.env.USER_ADDRESS as string
+
 async function getUserTransactions(userAddress: string): Promise<any> {
     const apiKey = API_KEY;
     const url = `https://api.covalenthq.com/v1/137/address/${userAddress}/transactions_v2/?key=${apiKey}`;
