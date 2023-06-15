af110c59e90930c6632121923c549fa45f3ca571
Shlok D. Mange <shlok_mange@protonmail.com>
2023-06-15 13:14:10 +0530

    base algo for determing a potential game score

---
+++ src/userScoreAlgorithm.ts
@@ -0,0 +1,11 @@
+const userScoringAlgorithm = async () => {
+    // fetch user transactions from db { include all mainnet chains [eth, polygon]}
+    // fetch user assets ( all chains, and calculate networth (spend potential))
+    // fetch nft assets ( 0.5 -> plain nfts, 0.75 -> nft games)
+    // recency multiplier ( num_transactions in the past 3 months higher weightage)
+
+    const score = 0
+    return score
+}
+
+export { userScoringAlgorithm }
