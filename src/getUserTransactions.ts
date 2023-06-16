00c6e22b6e8e70faf38b603b8cba86e9b328395b
Shlok D. Mange <shlok_mange@protonmail.com>
2023-06-16 17:05:17 +0800

    get User Transactions

--- src/getUserTransactions.ts
+++ src/getUserTransactions.ts
@@ -1,4 +1,3 @@
-import axios from 'axios'
 import { config } from 'dotenv'
 config()
 
@@ -7,10 +6,11 @@ const apiKey = process.env.COVALENT_API_KEY as string
 const getUserTransactions = async (userAddress: string, chainId: string): Promise<any> => {
     let headers = new Headers()
     headers.set('Authorization', `Bearer ${apiKey}`)
-    const url = `https://api.covalenthq.com/v1/${chainId}/address/${userAddress}/transactions_v3/?key=${apiKey}`
+    let url = `https://api.covalenthq.com/v1/${chainId}/address/${userAddress}/transactions_v3/`
     try {
-        const response = await axios.get(url)
-        return response.data.data.items
+        const response = await fetch(url, { method: 'GET', headers })
+        const data = await response.json()
+        return data
     } catch (error) {
         console.error(`Error: ${error}`)
     }
